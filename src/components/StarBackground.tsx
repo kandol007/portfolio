0bd6588edbfefffa9ab7c96eac4ca45464621ec4
Ritik Kumar <142918502+kandol007@users.noreply.github.com>
2025-07-01 10:16:42 +0530

    Update StarBackground.tsx

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -21,7 +21,7 @@ const StarBackground = (props: JSX.IntrinsicElements['group']) => {
   });
 
   return (
-    <group {...props}>
+    <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={positions} stride={3} frustumCulled>
         <PointMaterial
           transparent
